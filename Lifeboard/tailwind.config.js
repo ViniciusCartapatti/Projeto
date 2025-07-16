b6c648376f439ee94b802dc207bf8343dba742ed
viniciuscartapatti <viniciuscartapatti2@gmail.com>
2025-07-16 20:52:55 -0300

    Update tailwind.config.js

--- Lifeboard/tailwind.config.js
+++ Lifeboard/tailwind.config.js
@@ -1,14 +1,21 @@
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class", // <<< ADICIONE ESTA LINHA
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        dark: '#1a1a1a',
-        light: '#f5f5f5',
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
+export default config;
